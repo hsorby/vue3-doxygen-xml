6a14c2ac0ae959ceb788a63b57fcf67339617f55
Hugh Sorby <h.sorby@auckland.ac.nz>
2023-04-10 22:30:14 +1200

    Run linting on router/modules/doxygen.js.

--- src/router/modules/doxygen.js
+++ src/router/modules/doxygen.js
@@ -1,8 +1,9 @@
-export const getPageStem = routeTo => {
+export const getPageStem = (routeTo) => {
   let stem = routeTo.path
   if (routeTo.params.pageName) {
     const n = routeTo.path.lastIndexOf('/')
     stem = routeTo.path.slice(0, n)
   }
+
   return stem
 }
